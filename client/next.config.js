d63205d57e32a2d8d0d6cf77e6c789dfa196ce7f
Ashfaqe Ahmed <ashfaqe.sa12@gmail.com>
2023-05-19 23:47:23 +0530

    dep fix 11

--- client/next.config.js
+++ client/next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
     images: {
         domains: [
-                'res.cloudinary.com',
-                'avatars.githubusercontent.com',
-                'lh3.googleusercontent.com',
-                'a0.muscache.com',
-            ] //need to resolbe where images come from to use next Image component
-    }
-}
+            "res.cloudinary.com",
+            "avatars.githubusercontent.com",
+            "lh3.googleusercontent.com",
+            "a0.muscache.com",
+        ], //need to resolbe where images come from to use next Image component
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
