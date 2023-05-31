b7e527c72741d8160f10b6ca13709cc37171a6e5
Ashfaqe Ahmed <ashfaqe.sa12@gmail.com>
2023-05-31 20:27:22 +0530

    stripe checkouts attmpt 2.3

--- client/app/libs/stripe.ts
+++ client/app/libs/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 export const stripe = new Stripe(
     process.env.STRIPE_SECRET_KEY ?? '',
     {
-        apiVersion: '2020-08-27',
+        apiVersion: '2022-11-15',
         appInfo: {
             name: 'Landnd app',
             version: '0.1.0'
